b316fbe7206914aeade5a43880d4e0ea72f03b9f
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-07-21 23:51:23 +0300

    Add files via upload

---
+++ CSharpCourse/Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+                Console.WriteLine("Added!");
+        }
+        static void Add2(int number1,int number2)
+        {
+                Console.WriteLine("Subtracted!");
+        }
+    }
+}
