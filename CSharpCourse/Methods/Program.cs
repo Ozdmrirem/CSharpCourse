0323c01f3be14112d18a56316d1e31a10990678a
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-07-23 23:57:27 +0300

    Add files via upload

--- CSharpCourse/Methods/Program.cs
+++ CSharpCourse/Methods/Program.cs
@@ -10,17 +10,54 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Add();
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //var result = Add2();
+
+            //int number1;
+            //int number2 = 100;
+            //var result2 = Add3(out number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+
+            Console.WriteLine(multiply(2,4));
+            Console.WriteLine(multiply(2,4,5));
+
+            Console.WriteLine(Add4(1,2,3,4,5,6));
             Console.ReadLine();
         }
 
         static void Add()
         {
-                Console.WriteLine("Added!");
+            Console.WriteLine("Added!!!");
         }
-        static void Add2(int number1,int number2)
+        static int Add2(int number1 = 20, int number2 = 30)
         {
-                Console.WriteLine("Subtracted!");
+            var result = number1 + number2;
+            return result;
+
+        }
+
+        static int Add3(out int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+
+        static int multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        static int multiply(int number1, int number2,int number3)
+        {
+            return number1 * number2 * number3;
+        }   
+
+        static int Add4( params int[] numbers)
+        {
+            return numbers.Sum();
         }
     }
 }
---
+++ CSharpCourse/Strings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intro();
+            string sentence = "My name is İrem Özdemir";
+
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            //sentence = "My name is Emre Hanoğlu";
+
+            bool result3 = sentence.EndsWith("r");
+            bool result4 = sentence.StartsWith("My");
+
+            var result5 = sentence.IndexOf("name"); 
+            var result6 = sentence.IndexOf(" ");
+            var result7 = sentence.LastIndexOf(" ");
+            var result8 = sentence.Insert(0, "Hello, ");
+            var result9 = sentence.Substring(3);
+            Console.WriteLine(result9);
+            Console.ReadLine();
+        }
+
+        private static void Intro()
+        {
+            string city = "Ankara";
+            //Console.WriteLine(city[0]);
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            string city2 = "İstanbul";
+
+            Console.WriteLine(String.Format("{0} {1} ", city, city2));
+        }
+    }
+}
