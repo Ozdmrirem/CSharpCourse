2f2602a30d26a774212240d4d0329698c4d81bc7
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-08-06 18:36:37 +0300

    Add files via upload

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -11,19 +11,75 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            ArrayList cities = new ArrayList();
-
+            //ArrayList();
+            List<string> cities = new List<string>();
             cities.Add("Ankara");
-            cities.Add("Adana");
+            cities.Add("İstanbul");
 
             foreach (var city in cities)
             {
                 Console.WriteLine(city);
             }
 
-            cities.Add("İstanbul");
-            Console.WriteLine(cities[2]);
+            //List<Customer> customers = new List<Customer>();
+            //customers.Add(new Customer { Id = 1, FirstName = "Engin" });
+            //customers.Add(new Customer { Id = 2, FirstName = "Derin" });
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id = 1, FirstName = "Engin" },
+                new Customer { Id = 2, FirstName = "Derin" }
+            };
+
+            var count = customers.Count;
+            var customer2 = new Customer
+            {
+                Id = 3,FirstName = "Salih"
+            };
+
+            customers.Add(customer2);
+            customers.AddRange(new Customer[2]
+            {
+                new Customer { Id = 4, FirstName = "Ali" },
+                new Customer { Id = 5, FirstName = "Ayşe" }
+            });
+
+            customers.Clear();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+
             Console.ReadLine();
         }
+
+        private static void ArrayList()
+        {
+            ArrayList cities = new ArrayList();
+
+            cities.Add("Ankara");
+            cities.Add("Adana");
+
+
+            cities.Add("İstanbul");
+            cities.Add(1);
+            cities.Add('A');
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            //Console.WriteLine(cities[2]);
+        }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        
+       
     }
 }
