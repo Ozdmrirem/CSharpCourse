b973d01a01c418c635b3b6c4c243ba2b666c9fff
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-08-27 00:26:49 +0300

    Add files via upload

---
+++ Attributes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer{Id=1,LastName = "Demiroğ", Age= 32 };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+    }
+
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} Added!",
+                customer.Id,customer.FirstName,customer.LastName,customer.Age);
+        }
+    }
+}
