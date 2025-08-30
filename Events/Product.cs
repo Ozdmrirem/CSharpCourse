785b176b74bc8876752f8550691428803de6c63b
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-08-31 01:57:32 +0300

    Add files via upload

--- Events/Product.cs
+++ Events/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,25 @@ namespace Events
 
         public event StockControl StockControlEvent;
         public string ProductName { get; set; }
-        public int Stock { get; set; }
+        public int Stock
+        {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                _stock = value;
+                if (value<=15 && StockControlEvent!=null)
+                {
+                    StockControlEvent();
+                }
+            }
+        }
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine(" {1} Stock amount : {0}",Stock,ProductName);
+        }
     }
 }
